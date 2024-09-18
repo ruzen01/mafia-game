a1c0e4d7f606c5177e0c23ab77936a2f8358236b
ruzen01 <kim.vadim.alexandrovich@gmail.com>
2024-09-18 22:54:17 +1000

    add: rules and rating (controller, routes, method)

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\RatingController;
+use App\Http\Controllers\RulesController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -18,3 +20,6 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+Route::get('/rules', [RulesController::class, 'index'])->name('rules');
+Route::get('/rating', [RatingController::class, 'index'])->name('rules');
