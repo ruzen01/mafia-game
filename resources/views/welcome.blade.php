6d5c6d55d3544c1c477b6b978dc864bb680b97e0
ruzen <155291244+ruzen01@users.noreply.github.com>
2024-09-20 03:39:49 +0000

    add: fon to welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
 @section('title', 'Добро пожаловать')
 
 @section('content')
-    <div class="flex items-center justify-center min-h-screen text-center">
+    <div class="flex items-center justify-center min-h-screen text-center" style="background-image: url('https://drive.google.com/uc?export=view&id=19KbX-tA2KSz3mpH5wAp_c_4WQeNvFpma'); background-size: cover; background-position: center;">
         <div>
             <h1 class="text-5xl font-bold mb-4">Погрузитесь в мир Мафии</h1>
             <p class="text-xl mb-8">Где каждый ход может стать последним</p>
