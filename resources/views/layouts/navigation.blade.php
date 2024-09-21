a6c013e014474341071c5701db1f0907b9e998ef
ruzen01 <kim.vadim.alexandrovich@gmail.com>
2024-09-21 20:25:09 +1000

    fix: navigation

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -5,26 +5,29 @@
             <a href="{{ url('/') }}" class="hover:text-gray-400">Мафия</a>
         </div>
 
-        <!-- Центральная часть: Кнопки "Рейтинг" и "Дашборд" -->
+        <!-- Центральная часть: Кнопки "Рейтинг", "Игры", "Игроки" и "Дашборд" -->
         <div class="flex-1 text-center space-x-4">
-            <a href="{{ route('rating') }}" class="px-4 py-2 bg-blue-600 rounded hover:bg-blue-700">Рейтинг</a>
+            <a href="{{ route('rating') }}" class="px-4 py-2 rounded hover:bg-gray-700">Рейтинг</a>
+
+            <a href="{{ route('games.index') }}" class="px-4 py-2 rounded hover:bg-gray-700">Игры</a>
+            <a href="{{ route('players.index') }}" class="px-4 py-2 rounded hover:bg-gray-700">Игроки</a>
 
             @auth
-                <a href="{{ route('dashboard') }}" class="px-4 py-2 bg-green-600 rounded hover:bg-green-700">Дашборд</a>
+                <a href="{{ route('dashboard') }}" class="px-4 py-2 rounded hover:bg-gray-700">Дашборд</a>
             @endauth
         </div>
 
         <!-- Правая часть: Вход, Регистрация, Выход -->
         <div class="flex space-x-4">
             @guest
-                <a href="{{ route('login') }}" class="px-4 py-2 hover:text-gray-400">Вход</a>
+                <a href="{{ route('login') }}" class="px-4 py-2 hover:bg-gray-700">Вход</a>
                 <a href="{{ route('register') }}" class="px-4 py-2 bg-red-600 rounded hover:bg-red-700">Регистрация</a>
             @endguest
 
             @auth
                 <form method="POST" action="{{ route('logout') }}">
                     @csrf
-                    <button type="submit" class="px-4 py-2 bg-red-600 rounded hover:bg-red-700">
+                    <button type="submit" class="px-4 py-2 bg-gray-700 rounded hover:bg-red-700">
                         Выход
                     </button>
                 </form>
