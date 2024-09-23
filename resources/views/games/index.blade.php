19ed63a224d9dcb37a7107f0ab3f8b3db454a3d7
ruzen <155291244+ruzen01@users.noreply.github.com>
2024-09-23 03:38:08 +0000

    fix: game view

--- resources/views/games/index.blade.php
+++ resources/views/games/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<div class="container mx-auto">
     <h1 class="text-center text-3xl mb-6">Список игр</h1>
 
     <!-- Ссылка на создание новой игры -->
@@ -12,13 +12,13 @@
     </div>
 
     <!-- Таблица со списком игр -->
-    <div class="flex justify-center">
-        <table class="table-auto border-collapse border border-gray-500 w-3/4">
+    <div class="overflow-x-auto">
+        <table class="table-auto border-collapse border border-gray-500 w-full">
             <thead>
                 <tr class="bg-gray-200">
-                    <th class="border border-gray-400 px-4 py-2">Имя</th>
                     <th class="border border-gray-400 px-4 py-2">Дата</th>
-                    <th class="border border-gray-400 px-4 py-2">Номер игры</th>
+                    <th class="border border-gray-400 px-4 py-2">Имя</th>
+                    <th class="border border-gray-400 px-4 py-2 w-20">Номер игры</th>
                     <th class="border border-gray-400 px-4 py-2">Ведущий</th>
                     <th class="border border-gray-400 px-4 py-2">Победитель</th>
                     <th class="border border-gray-400 px-4 py-2">Игроки</th>
@@ -28,25 +28,36 @@
             <tbody>
                 @foreach($games as $game)
                 <tr>
+                    <!-- Дата игры (в формате день-месяц-год) -->
+                    <td class="border border-gray-400 px-4 py-2">{{ \Carbon\Carbon::parse($game->date)->format('d.m.Y') }}</td>
+
                     <!-- Ссылка на страницу с подробной информацией об игре -->
                     <td class="border border-gray-400 px-4 py-2">
                         <a href="{{ route('games.show', $game->id) }}" class="text-blue-500 underline">
                             {{ $game->name }}
                         </a>
                     </td>
-                    <td class="border border-gray-400 px-4 py-2">{{ $game->date }}</td>
-                    <td class="border border-gray-400 px-4 py-2">{{ $game->game_number }}</td>
+
+                    <!-- Номер игры (с узкой шириной) -->
+                    <td class="border border-gray-400 px-4 py-2 text-center w-20">{{ $game->game_number }}</td>
+
+                    <!-- Ведущий игры -->
                     <td class="border border-gray-400 px-4 py-2">{{ $game->host_name }}</td>
+
+                    <!-- Победитель игры -->
                     <td class="border border-gray-400 px-4 py-2">{{ $game->winner }}</td>
-                    <td class="border border-gray-400 px-4 py-2">
+
+                    <!-- Игроки игры (с переносом длинного текста) -->
+                    <td class="border border-gray-400 px-4 py-2 break-words max-w-xs">
                         @if ($game->players)
-                        {{ $game->players->pluck('name')->implode(', ') }}
+                            {{ $game->players->pluck('name')->implode(', ') }}
                         @else
-                        Игроки не указаны
+                            Игроки не указаны
                         @endif
                     </td>
-                    <td class="border border-gray-400 px-4 py-2">
-                        <!-- Кнопка редактирования игры -->
+
+                    <!-- Кнопка редактирования игры -->
+                    <td class="border border-gray-400 px-4 py-2 text-center">
                         <a href="{{ route('games.edit', $game->id) }}" class="bg-yellow-500 text-white py-1 px-2 rounded">
                             Изменить
                         </a>
