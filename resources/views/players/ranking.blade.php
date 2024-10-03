0139750cda542254b0a017b20c0ac692aabcbc12
ruzen 01 <kim.vadim.alexandrovich@gmail.com>
2024-10-03 06:43:54 +0000

    fix: experimental css

--- resources/views/players/ranking.blade.php
+++ resources/views/players/ranking.blade.php
@@ -9,7 +9,7 @@
         <table class="min-w-full table-auto shadow-lg rounded-lg overflow-hidden border-collapse border border-gray-500">
             <thead class="bg-gray-700 text-white">
                 <tr>
-                    <th class="sticky top-0 z-10 border border-gray-400 px-4 py-2 bg-gray-300 text-center">#</th> <!-- Новый столбец для номера -->
+                    <th class="sticky top-0 z-10 border border-gray-400 px-4 py-2 bg-gray-300 text-center">#</th>
                     <th class="sticky top-0 z-10 border border-gray-400 px-4 py-2 bg-yellow-200 text-center">Игрок</th>
                     <th class="sticky top-0 z-10 border border-gray-400 px-4 py-2 bg-pink-200 text-center">Σ Баллов</th>
                     <th class="sticky top-0 z-10 border border-gray-400 px-4 py-2 bg-blue-200 text-center">Σ Игр</th>
@@ -21,8 +21,8 @@
             </thead>
             <tbody>
                 @foreach($players as $player)
-                <tr>
-                    <td class="border border-gray-400 px-4 py-2 text-center">{{ $loop->iteration }}</td> <!-- Порядковый номер -->
+                <tr class="{{ $loop->iteration == 1 ? 'bg-yellow-600' : ($loop->iteration == 2 ? 'bg-gray-400' : ($loop->iteration == 3 ? 'bg-yellow-800' : 'bg-blue-'.(900 - $loop->iteration * 10))) }}">
+                    <td class="border border-gray-400 px-4 py-2 text-center">{{ $loop->iteration }}</td>
                     <td class="border border-gray-400 px-4 py-2 text-center">{{ $player->name }}</td>
                     <td class="border border-gray-400 px-4 py-2 text-center">{{ $player->games->sum('pivot.score') }}</td>
                     <td class="border border-gray-400 px-4 py-2 text-center">{{ $player->total_games }}</td>
