a20b4af823afcea37fb96520c0e846af33bee4bc
ruzen 01 <kim.vadim.alexandrovich@gmail.com>
2024-10-03 06:29:41 +0000

    fix: style

--- resources/views/players/ranking.blade.php
+++ resources/views/players/ranking.blade.php
@@ -8,19 +8,19 @@
         <table class="table-auto shadow-lg rounded-lg overflow-hidden border-collapse border border-gray-500 w-full">
             <thead class="bg-gray-700 text-white">
                 <tr>
-                    <th class="sticky top-0 border border-gray-400 px-4 py-2 bg-yellow-200 text-center">Игрок</th>
-                    <th class="sticky top-0 border border-gray-400 px-4 py-2 bg-pink-200 text-center">Σ Баллов</th>
-                    <th class="sticky top-0 border border-gray-400 px-4 py-2 bg-blue-200 text-center">Σ Игр</th>
-                    <th class="sticky top-0 border border-gray-400 px-4 py-2 bg-green-200 text-center">Σ Побед</th>
-                    <th class="sticky top-0 border border-gray-400 px-4 py-2 bg-orange-200 text-center">Σ Лучших</th>
-                    <th class="sticky top-0 border border-gray-400 px-4 py-2 bg-purple-200 text-center">Σ Первых жертв</th>
-                    <th class="sticky top-0 border border-gray-400 px-4 py-2 bg-red-200 text-center">Σ Доп. баллов</th>
+                    <th class="sticky top-0 border border-gray-400 px-4 py-2 bg-yellow-600 text-left">Игрок</th>
+                    <th class="sticky top-0 border border-gray-400 px-4 py-2 bg-pink-600 text-center">Σ Баллов</th>
+                    <th class="sticky top-0 border border-gray-400 px-4 py-2 bg-blue-600 text-center">Σ Игр</th>
+                    <th class="sticky top-0 border border-gray-400 px-4 py-2 bg-green-600 text-center">Σ Побед</th>
+                    <th class="sticky top-0 border border-gray-400 px-4 py-2 bg-orange-600 text-center">Σ Лучших</th>
+                    <th class="sticky top-0 border border-gray-400 px-4 py-2 bg-purple-600 text-center">Σ Первых жертв</th>
+                    <th class="sticky top-0 border border-gray-400 px-4 py-2 bg-red-600 text-center">Σ Доп. баллов</th>
                 </tr>
             </thead>
             <tbody>
                 @foreach($players as $player)
                 <tr>
-                    <td class="border border-gray-400 px-4 py-2 text-center">{{ $player->name }}</td>
+                    <td class="border border-gray-400 px-4 py-2 text-left">{{ $player->name }}</td>
                     <td class="border border-gray-400 px-4 py-2 text-center">{{ $player->games->sum('pivot.score') }}</td>
                     <td class="border border-gray-400 px-4 py-2 text-center">{{ $player->total_games }}</td>
                     <td class="border border-gray-400 px-4 py-2 text-center">{{ $player->games->where('pivot.score', '>=', 2)->count() }}</td>
