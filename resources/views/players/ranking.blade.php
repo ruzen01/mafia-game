10664d5ac8e9fa85007b5b8fe0589768f489036e
ruzen 01 <kim.vadim.alexandrovich@gmail.com>
2024-10-03 06:22:16 +0000

    fix: style

--- resources/views/players/ranking.blade.php
+++ resources/views/players/ranking.blade.php
@@ -7,7 +7,7 @@
     <table class="table-auto shadow-lg rounded-lg overflow-hidden border-collapse border border-gray-500 w-full">
         <thead class="bg-gray-700 text-white">
             <tr>
-                <th class="border border-gray-400 px-4 py-2 bg-yellow-500">Игрок</th>
+                <th class="border border-gray-400 px-4 py-2 bg-yellow-500 text-left">Игрок</th>
                 <th class="border border-gray-400 px-4 py-2 bg-pink-500 text-center">Σ Баллов</th>
                 <th class="border border-gray-400 px-4 py-2 bg-blue-500 text-center">Σ Игр</th>
                 <th class="border border-gray-400 px-4 py-2 bg-green-500 text-center">Σ Побед</th>
