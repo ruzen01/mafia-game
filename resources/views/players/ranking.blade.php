a93ea2905b4b4fe3d41930411e164a1d40675648
ruzen 01 <kim.vadim.alexandrovich@gmail.com>
2024-10-03 06:57:06 +0000

    fix

--- resources/views/players/ranking.blade.php
+++ resources/views/players/ranking.blade.php
@@ -7,16 +7,16 @@
     <!-- Контейнер с фиксированной высотой и вертикальной прокруткой -->
     <div class="overflow-y-scroll border border-gray-400">
         <table class="min-w-full table-auto shadow-lg rounded-lg border-collapse border border-gray-500">
-            <thead class="fixed bg-gray-700 text-white">
+            <thead class="bg-gray-700 text-white">
                 <tr>
-                    <th class="border border-gray-400 px-4 py-2 bg-gray-300 text-center">#</th> <!-- Новый столбец для номера -->
-                    <th class="border border-gray-400 px-4 py-2 bg-yellow-500 text-center">Игрок</th>
-                    <th class="border border-gray-400 px-4 py-2 bg-pink-500 text-center">Σ Баллов</th>
-                    <th class="border border-gray-400 px-4 py-2 bg-blue-500 text-center">Σ Игр</th>
-                    <th class="border border-gray-400 px-4 py-2 bg-green-500 text-center">Σ Побед</th>
-                    <th class="border border-gray-400 px-4 py-2 bg-orange-500 text-center">Σ Лучших</th>
-                    <th class="border border-gray-400 px-4 py-2 bg-purple-500 text-center">Σ Первых жертв</th>
-                    <th class="border border-gray-400 px-4 py-2 bg-red-500 text-center">Σ Доп. баллов</th>
+                    <th class="fixed border border-gray-400 px-4 py-2 bg-gray-300 text-center">#</th> <!-- Новый столбец для номера -->
+                    <th class="fixed border border-gray-400 px-4 py-2 bg-yellow-500 text-center">Игрок</th>
+                    <th class="fixed border border-gray-400 px-4 py-2 bg-pink-500 text-center">Σ Баллов</th>
+                    <th class="fixed border border-gray-400 px-4 py-2 bg-blue-500 text-center">Σ Игр</th>
+                    <th class="fixed border border-gray-400 px-4 py-2 bg-green-500 text-center">Σ Побед</th>
+                    <th class="fixed border border-gray-400 px-4 py-2 bg-orange-500 text-center">Σ Лучших</th>
+                    <th class="fixed border border-gray-400 px-4 py-2 bg-purple-500 text-center">Σ Первых жертв</th>
+                    <th class="fixed border border-gray-400 px-4 py-2 bg-red-500 text-center">Σ Доп. баллов</th>
                 </tr>
             </thead>
             <tbody>
