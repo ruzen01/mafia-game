4bf8fac3462037db76b7c4d07f924abaa51b2c45
ruzen 01 <kim.vadim.alexandrovich@gmail.com>
2024-10-03 06:19:46 +0000

    add: style

--- resources/views/players/ranking.blade.php
+++ resources/views/players/ranking.blade.php
@@ -7,27 +7,25 @@
     <table class="table-auto shadow-lg rounded-lg overflow-hidden border-collapse border border-gray-500 w-full">
         <thead class="bg-gray-700 text-white">
             <tr>
-                <th class="border border-gray-400 px-4 py-2">Игрок</th>
-                <th class="border border-gray-400 px-4 py-2">Баллы</th>
-                <th class="border border-gray-400 px-4 py-2">Количество игр</th>
-                <th class="border border-gray-400 px-4 py-2">Количество побед</th>
-                <th class="border border-gray-400 px-4 py-2">Количество поражений</th>
-                <th class="border border-gray-400 px-4 py-2">Баллы за лучшего</th>
-                <th class="border border-gray-400 px-4 py-2">Баллы за первую жертву</th>
-                <th class="border border-gray-400 px-4 py-2">Дополнительные баллы</th>
+                <th class="border border-gray-400 px-4 py-2 bg-yellow-200 text-center">Игрок</th>
+                <th class="border border-gray-400 px-4 py-2 bg-pink-200 text-center">Σ Баллов</th>
+                <th class="border border-gray-400 px-4 py-2 bg-blue-200 text-center">Σ Игр</th>
+                <th class="border border-gray-400 px-4 py-2 bg-green-200 text-center">Σ Побед</th>
+                <th class="border border-gray-400 px-4 py-2 bg-orange-200 text-center">Σ Лучших</th>
+                <th class="border border-gray-400 px-4 py-2 bg-purple-200 text-center">Σ Первых жертв</th>
+                <th class="border border-gray-400 px-4 py-2 bg-red-200 text-center">Σ Доп. баллов</th>
             </tr>
         </thead>
         <tbody>
             @foreach($players as $player)
             <tr>
-                <td class="border border-gray-400 px-4 py-2">{{ $player->name }}</td>
-                <td class="border border-gray-400 px-4 py-2">{{ $player->games->sum('pivot.score') }}</td>
-                <td class="border border-gray-400 px-4 py-2">{{ $player->total_games }}</td>
-                <td class="border border-gray-400 px-4 py-2">{{ $player->games->where('pivot.score', '>=', 2)->count() }}</td>
-                <td class="border border-gray-400 px-4 py-2">{{ $player->games->where('pivot.score', '<', 2)->count() }}</td>
-                <td class="border border-gray-400 px-4 py-2">{{ $player->games->where('pivot.best_player', 1)->count() }}</td>
-                <td class="border border-gray-400 px-4 py-2">{{ $player->games->where('pivot.first_victim', 1)->count() }}</td>
-                <td class="border border-gray-400 px-4 py-2">{{ $player->games->sum('pivot.additional_score') }}</td>
+                <td class="border border-gray-400 px-4 py-2 text-center">{{ $player->name }}</td>
+                <td class="border border-gray-400 px-4 py-2 text-center">{{ $player->games->sum('pivot.score') }}</td>
+                <td class="border border-gray-400 px-4 py-2 text-center">{{ $player->total_games }}</td>
+                <td class="border border-gray-400 px-4 py-2 text-center">{{ $player->games->where('pivot.score', '>=', 2)->count() }}</td>
+                <td class="border border-gray-400 px-4 py-2 text-center">{{ $player->games->where('pivot.best_player', 1)->count() }}</td>
+                <td class="border border-gray-400 px-4 py-2 text-center">{{ $player->games->where('pivot.first_victim', 1)->count() }}</td>
+                <td class="border border-gray-400 px-4 py-2 text-center">{{ $player->games->sum('pivot.additional_score') }}</td>
             </tr>
             @endforeach
         </tbody>
