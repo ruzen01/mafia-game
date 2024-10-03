d51358784a34268382c8ac73e3dede286bee07b6
ruzen 01 <kim.vadim.alexandrovich@gmail.com>
2024-10-03 06:36:50 +0000

    fix style

--- resources/views/players/ranking.blade.php
+++ resources/views/players/ranking.blade.php
@@ -5,17 +5,17 @@
     <h1 class="text-3xl font-bold mb-6 text-center">Рейтинг игроков</h1>
 
     <!-- Контейнер с фиксированной высотой и вертикальной прокруткой -->
-    <div class="overflow-y-auto max-h-96">
-        <table class="table-auto shadow-lg rounded-lg overflow-hidden border-collapse border border-gray-500 w-full">
-            <thead class="sticky top-0 bg-gray-700 text-white">
+    <div class="overflow-y-scroll max-h-[500px] border border-gray-400">
+        <table class="min-w-full table-auto shadow-lg rounded-lg border-collapse border border-gray-500">
+            <thead class="bg-gray-700 text-white">
                 <tr>
-                    <th class="sticky top-0 border border-gray-400 px-4 py-2 bg-yellow-200 text-center">Игрок</th>
-                    <th class="sticky top-0 border border-gray-400 px-4 py-2 bg-pink-200 text-center">Σ Баллов</th>
-                    <th class="sticky top-0 border border-gray-400 px-4 py-2 bg-blue-200 text-center">Σ Игр</th>
-                    <th class="sticky top-0 border border-gray-400 px-4 py-2 bg-green-200 text-center">Σ Побед</th>
-                    <th class="sticky top-0 border border-gray-400 px-4 py-2 bg-orange-200 text-center">Σ Лучших</th>
-                    <th class="sticky top-0 border border-gray-400 px-4 py-2 bg-purple-200 text-center">Σ Первых жертв</th>
-                    <th class="sticky top-0 border border-gray-400 px-4 py-2 bg-red-200 text-center">Σ Доп. баллов</th>
+                    <th class="sticky top-0 z-10 border border-gray-400 px-4 py-2 bg-yellow-200 text-center">Игрок</th>
+                    <th class="sticky top-0 z-10 border border-gray-400 px-4 py-2 bg-pink-200 text-center">Σ Баллов</th>
+                    <th class="sticky top-0 z-10 border border-gray-400 px-4 py-2 bg-blue-200 text-center">Σ Игр</th>
+                    <th class="sticky top-0 z-10 border border-gray-400 px-4 py-2 bg-green-200 text-center">Σ Побед</th>
+                    <th class="sticky top-0 z-10 border border-gray-400 px-4 py-2 bg-orange-200 text-center">Σ Лучших</th>
+                    <th class="sticky top-0 z-10 border border-gray-400 px-4 py-2 bg-purple-200 text-center">Σ Первых жертв</th>
+                    <th class="sticky top-0 z-10 border border-gray-400 px-4 py-2 bg-red-200 text-center">Σ Доп. баллов</th>
                 </tr>
             </thead>
             <tbody>
