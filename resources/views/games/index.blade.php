f95fffb943c1a08b198d517e5160fac54731ec4b
ruzen 01 <kim.vadim.alexandrovich@gmail.com>
2024-10-04 02:45:42 +0000

    css

--- resources/views/games/index.blade.php
+++ resources/views/games/index.blade.php
@@ -15,7 +15,7 @@
         </div>
     @endif
 
-    <h1 class= text-3xl font-bold mb-6">Список игр</h1>
+    <h1 class="text-left text-3xl font-bold mb-6">Список игр</h1>
 
     @can('create', App\Models\Game::class)
     <div class="flex justify-center mb-6">
@@ -29,15 +29,15 @@
         <table class="table-fixed w-full">
             <thead class="bg-gray-700 text-white sticky top-0 z-10">
                 <tr>
-                    <th class="truncate w-1/16 px-4 py-2">Дата</th>
-                    <th class="truncate w-4/16 px-4 py-2">Имя</th>
-                    <th class="truncate w-1/16 px-4 py-2">№</th>
-                    <th class="truncate w-2/16 px-4 py-2">Ведущий</th>
-                    <th class="truncate w-2/16 px-4 py-2">Сезон</th>
-                    <th class="truncate w-2/16 px-4 py-2">Победитель</th>
-                    <th class="truncate w-2/16 px-4 py-2">Игроки</th>
+                    <th class="truncate w-1/16 px-4 py-2 text-left">Дата</th>
+                    <th class="truncate w-4/16 px-4 py-2 text-left">Имя</th>
+                    <th class="truncate w-1/16 px-4 py-2 text-left">№</th>
+                    <th class="truncate w-2/16 px-4 py-2 text-left">Ведущий</th>
+                    <th class="truncate w-2/16 px-4 py-2 text-left">Сезон</th>
+                    <th class="truncate w-2/16 px-4 py-2 text-left">Победитель</th>
+                    <th class="truncate w-2/16 px-4 py-2 text-left">Игроки</th>
                     @can('update', App\Models\Game::class)
-                    <th class="truncate w-2/16 px-4 py-2">Действия</th>
+                    <th class="truncate w-2/16 px-4 py-2 text-left">Действия</th>
                     @endcan
                 </tr>
             </thead>
@@ -78,7 +78,7 @@
 </td>
 
                     @can('update', [$game])
-                    <td class="w-2/16 px-4 py-1 whitespace-nowrap">
+                    <td class="w-2/16 px-4 py-1 text-left whitespace-nowrap">
                         <form action="{{ route('games.edit', $game->id) }}" method="GET" style="display:inline-block;">
                             <button type="submit" class="bg-yellow-500 text-white py-1 px-2 rounded">Изменить</button>
                         </form>
