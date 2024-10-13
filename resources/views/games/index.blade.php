fd4404267c268cc4894ba9fc1267b2c27357be77
ruzen 01 <kim.vadim.alexandrovich@gmail.com>
2024-10-13 23:36:46 +0000

    css

--- resources/views/games/index.blade.php
+++ resources/views/games/index.blade.php
@@ -14,7 +14,7 @@
     </div>
     @endif
 
-    <h1 class="text-center text-3xl font-bold mb-6">Список игр</h1>
+    <h1 class="text-center text-4xl font-extrabold text-gray-800 mb-6">Список игр</h1> <!-- Увеличил размер заголовка и сделал его темнее -->
 
     @can('create', App\Models\Game::class)
     <div class="flex justify-right mb-6">
@@ -26,34 +26,34 @@
 
     <div class="overflow-x-auto rounded-lg shadow-lg">
         <table class="table-auto w-full">
-            <thead class="sticky top-0 z-10">
+            <thead class="bg-gray-300 text-gray-900 text-left"> <!-- Темнее для заголовков таблицы -->
                 <tr>
-                    <th class="truncate px-4 py-2 text-left">Дата</th>
-                    <th class="truncate px-4 py-2 text-left">Имя</th>
-                    <th class="truncate px-4 py-2 text-left">№</th>
-                    <th class="truncate px-4 py-2 text-left">Ведущий</th>
-                    <th class="truncate px-4 py-2 text-left">Сезон</th>
-                    <th class="truncate px-4 py-2 text-left">Победитель</th>
-                    <th class="truncate px-4 py-2 text-left">Игроки</th>
+                    <th class="truncate px-4 py-2">Дата</th>
+                    <th class="truncate px-4 py-2">Имя</th>
+                    <th class="truncate px-4 py-2">№</th>
+                    <th class="truncate px-4 py-2">Ведущий</th>
+                    <th class="truncate px-4 py-2">Сезон</th>
+                    <th class="truncate px-4 py-2">Победитель</th>
+                    <th class="truncate px-4 py-2">Игроки</th>
                     @can('update', App\Models\Game::class)
-                    <th class="truncate px-4 py-2 text-left">Действия</th>
+                    <th class="truncate px-4 py-2">Действия</th>
                     @endcan
                 </tr>
             </thead>
             <tbody>
                 @foreach($games as $game)
-                <tr class="odd:bg-gray-100 even:bg-gray-200">
-                    <td class="truncate px-4 py-1">{{ \Carbon\Carbon::parse($game->date)->format('d.m.Y') }}</td>
-                    <td class="truncate max-w-xs px-4 py-1">
+                <tr class="odd:bg-gray-100 even:bg-gray-200 hover:bg-gray-300"> <!-- Чередующиеся строки с легким выделением при наведении -->
+                    <td class="truncate px-4 py-2">{{ \Carbon\Carbon::parse($game->date)->format('d.m.Y') }}</td>
+                    <td class="truncate max-w-xs px-4 py-2">
                         <a href="{{ route('games.show', $game->id) }}" class="hover:text-blue-500">
                             {{ $game->name }}
                         </a>
                     </td>
-                    <td class="truncate px-4 py-1">{{ $game->game_number }}</td>
-                    <td class="truncate px-4 py-1">{{ $game->host_name }}</td>
-                    <td class="truncate px-4 py-1">{{ $game->season }}</td>
-                    <td class="truncate px-4 py-1">{{ $game->winner }}</td>
-                    <td class="truncate px-4 py-1">
+                    <td class="truncate px-4 py-2">{{ $game->game_number }}</td>
+                    <td class="truncate px-4 py-2">{{ $game->host_name }}</td>
+                    <td class="truncate px-4 py-2">{{ $game->season }}</td>
+                    <td class="truncate px-4 py-2">{{ $game->winner }}</td>
+                    <td class="truncate px-4 py-2">
                         <div class="flex -space-x-2">
                             @foreach($game->players->take(5) as $player)
                             @php
@@ -63,7 +63,7 @@
                             $initials .= strtoupper(substr(explode(' ', $player->name)[1], 0, 1));
                             }
                             @endphp
-                            <div class="relative group flex items-center justify-center text-white font-bold w-6 h-6">
+                            <div class="relative group flex items-center justify-center text-white font-bold w-6 h-6 rounded-full bg-blue-500">
                                 {{ $initials }}
                                 <span class="absolute bottom-12 left-1/2 transform -translate-x-1/2 bg-black text-white text-xs rounded py-1 px-2 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
                                     {{ $player->name }}
@@ -72,21 +72,21 @@
                             @endforeach
 
                             @if($game->players->count() > 5)
-                            <span class="text-white text-xs font-bold ml-4">
+                            <span class="text-gray-700 text-xs font-bold ml-4">
                                 +{{ $game->players->count() - 5 }}
                             </span>
                             @endif
                         </div>
                     </td>
                     @can('update', [$game])
-                    <td class="truncate px-4 py-1">
+                    <td class="truncate px-4 py-2">
                         <form action="{{ route('games.edit', $game->id) }}" method="GET" style="display:inline-block;">
-                            <button type="submit" class="bg-yellow-500 text-white py-1 px-2 rounded">Изменить</button>
+                            <button type="submit" class="bg-yellow-500 text-white py-1 px-2 rounded hover:bg-yellow-400">Изменить</button>
                         </form>
                         <form action="{{ route('games.destroy', $game->id) }}" method="POST" style="display:inline-block;">
                             @csrf
                             @method('DELETE')
-                            <button type="submit" class="bg-red-500 text-white py-1 px-2 rounded" onclick="return confirm('Вы уверены, что хотите удалить эту игру?')">
+                            <button type="submit" class="bg-red-500 text-white py-1 px-2 rounded hover:bg-red-400" onclick="return confirm('Вы уверены, что хотите удалить эту игру?')">
                                 Удалить
                             </button>
                         </form>
