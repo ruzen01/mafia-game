2a0cda35be2ced657933ef3d1e9468d3c88dea1b
ruzen 01 <kim.vadim.alexandrovich@gmail.com>
2025-02-14 01:52:32 +0000

    upd welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,14 +1,16 @@
 @extends('layouts.app')
-
 @section('title', 'Добро пожаловать')
-
 @section('content')
     <div class="flex items-center justify-center text-center">
-        <div>
+        <div class="grid gap-4"> <!-- grid создает сетку, gap-4 добавляет отступы -->
             <h1 class="text-5xl font-bold mb-4">Погрузитесь в мир Мафии</h1>
             <p class="text-xl mb-8">Где каждый ход может стать последним</p>
-            <a href="{{ route('rules') }}" class="bg-gray-800 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded block">Правила</a>
-            <a href="{{ route('roles') }}" class="bg-gray-800 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded block">Роли</a>
+            <a href="{{ route('rules') }}" class="bg-gray-800 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded w-full">
+                Правила
+            </a>
+            <a href="{{ route('roles') }}" class="bg-gray-800 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded w-full">
+                Роли
+            </a>
         </div>
     </div>
 @endsection
