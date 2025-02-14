ce858c51653a71c7ed483a8cbeea3b14c19846ae
ruzen 01 <kim.vadim.alexandrovich@gmail.com>
2025-02-14 01:51:01 +0000

    upd welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,8 +7,8 @@
         <div>
             <h1 class="text-5xl font-bold mb-4">Погрузитесь в мир Мафии</h1>
             <p class="text-xl mb-8">Где каждый ход может стать последним</p>
-            <a href="{{ route('rules') }}" class="bg-gray-800 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded">Правила</a>
-            <a href="{{ route('roles') }}" class="bg-gray-800 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded">Роли</a>
+            <a href="{{ route('rules') }}" class="bg-gray-800 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded block">Правила</a>
+            <a href="{{ route('roles') }}" class="bg-gray-800 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded block">Роли</a>
         </div>
     </div>
 @endsection
