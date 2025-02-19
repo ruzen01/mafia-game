df28dc6f4cb28afcacc65c7cbcee8ed298ed7328
ruzen 01 <kim.vadim.alexandrovich@gmail.com>
2025-02-19 23:54:53 +0000

    add

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,8 +5,3 @@ import Alpine from 'alpinejs';
 window.Alpine = Alpine;
 
 Alpine.start();
-
-// import function to register Swiper custom elements
-import { register } from 'swiper/element/bundle';
-// register Swiper custom elements
-register();
