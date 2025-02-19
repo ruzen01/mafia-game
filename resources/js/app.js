88524ecca2685b6708415d8d4d02d06140a8ee94
ruzen 01 <kim.vadim.alexandrovich@gmail.com>
2025-02-19 23:32:31 +0000

    add

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,33 +6,7 @@ window.Alpine = Alpine;
 
 Alpine.start();
 
-// Импорт основного модуля Swiper
-import Swiper from 'swiper';
-
-// Импорт необходимых модулей (Navigation и Pagination)
-import { Navigation, Pagination } from 'swiper/modules';
-
-// Импорт CSS для Swiper
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-
-// Подключение модулей к Swiper
-Swiper.use([Navigation, Pagination]);
-
-// Инициализация Swiper
-document.addEventListener('DOMContentLoaded', () => {
-    new Swiper('.mySwiper', {
-        slidesPerView: 1, // Количество слайдов на экране
-        spaceBetween: 20, // Пробел между слайдами
-        loop: true, // Бесконечный цикл
-        navigation: {
-            nextEl: '.swiper-button-next', // Селектор кнопки "вперед"
-            prevEl: '.swiper-button-prev', // Селектор кнопки "назад"
-        },
-        pagination: {
-            el: '.swiper-pagination', // Селектор пагинации
-            clickable: true, // Пагинация кликабельна
-        },
-    });
-});
+// import function to register Swiper custom elements
+import { register } from 'swiper/element/bundle';
+// register Swiper custom elements
+register();
