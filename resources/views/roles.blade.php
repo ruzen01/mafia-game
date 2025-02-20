c21da9f54ce058fa5c0d8ba6da8f459d979824ba
ruzen 01 <kim.vadim.alexandrovich@gmail.com>
2025-02-20 03:17:13 +0000

    add

--- resources/views/roles.blade.php
+++ resources/views/roles.blade.php
@@ -21,7 +21,7 @@
                         <img src="{{ asset('images/roles/advokat.png') }}" alt="Адвокат" class="w-full h-full object-cover rounded-lg border border-gray-300">
                     </div>
                     <div class="w-full md:w-1/2 md:pl-8 text-left">
-                        <h3 class="text-xl font-bold mb-4 text-center md:text-left">Адвокат</h3>
+                        <h3 class="text-xl font-bold mb-4">Адвокат</h3>
                         <p class="text-gray-700 text-sm">
                             Сторона: Мафия<br>
                             Проверка: <span class="text-red-500">Мирный житель</span><br>
@@ -41,7 +41,7 @@
                         <img src="{{ asset('images/roles/actor.png') }}" alt="Актёр" class="w-full h-full object-cover rounded-lg border border-gray-300">
                     </div>
                     <div class="w-full md:w-1/2 md:pl-8 text-left">
-                        <h3 class="text-xl font-bold mb-4 text-center md:text-left">Актёр</h3>
+                        <h3 class="text-xl font-bold mb-4">Актёр</h3>
                         <p class="text-gray-700 text-sm">
                             Сторона: Мирные<br>
                             Проверка: <span class="text-red-500">Мирный житель</span><br>
@@ -60,7 +60,7 @@
                         <img src="{{ asset('images/roles/bessmertniy.png') }}" alt="Бессмертный" class="w-full h-full object-cover rounded-lg border border-gray-300">
                     </div>
                     <div class="w-full md:w-1/2 md:pl-8 text-left">
-                        <h3 class="text-xl font-bold mb-4 text-center md:text-left">Бессмертный</h3>
+                        <h3 class="text-xl font-bold mb-4">Бессмертный</h3>
                         <p class="text-gray-700 text-sm">
                             Сторона: Мирные<br>
                             Проверка: <span class="text-red-500">Мирный житель</span><br>
@@ -79,7 +79,7 @@
                         <img src="{{ asset('images/roles/briker.png') }}" alt="Брокер" class="w-full h-full object-cover rounded-lg border border-gray-300">
                     </div>
                     <div class="w-full md:w-1/2 md:pl-8 text-left">
-                        <h3 class="text-xl font-bold mb-4 text-center md:text-left">Брокер</h3>
+                        <h3 class="text-xl font-bold mb-4">Брокер</h3>
                         <p class="text-gray-700 text-sm">
                             Сторона: Мирные<br>
                             Проверка: <span class="text-red-500">Мирный житель</span><br>
