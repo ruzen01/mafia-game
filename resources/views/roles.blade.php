dcb8f0629658c191221a34762c261dda18a6d460
ruzen 01 <kim.vadim.alexandrovich@gmail.com>
2025-02-20 03:24:14 +0000

    add

--- resources/views/roles.blade.php
+++ resources/views/roles.blade.php
@@ -13,7 +13,7 @@
             pagination="true"
             pagination-clickable="true"
             pagination-dynamic-bullets="true"
-            data-swiper-parallax="true"
+            data-swiper-parallax="-300"
         >
             <!-- Карточка Адвоката -->
             <swiper-slide class="relative bg-white rounded-lg shadow-md overflow-hidden">
