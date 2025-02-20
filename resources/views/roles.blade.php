691f11b32e80c0a69d347496727a4e5746e96403
ruzen 01 <kim.vadim.alexandrovich@gmail.com>
2025-02-20 23:24:48 +0000

    add

--- resources/views/roles.blade.php
+++ resources/views/roles.blade.php
@@ -93,8 +93,8 @@
             </swiper-slide>
 
             <!-- Карточка Взломщик -->
-            <swiper-slide class="relative bg-white rounded-lg shadow-md overflow-hidden filter sepia">
-                <div class="p-6 flex flex-col md:flex-row items-center">
+            <swiper-slide class="relative bg-white rounded-lg shadow-md overflow-hidden">
+                <div class="p-6 flex flex-col md:flex-row items-center filter sepia">
                     <div class="w-full md:w-1/2 h-70 md:h-auto">
                         <img src="{{ asset('images/roles/vzlomshik.png') }}" alt="Взломщик" class="w-full h-full object-cover rounded-lg border border-gray-300">
                     </div>
