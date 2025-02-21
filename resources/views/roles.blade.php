2f367d5b0aff3d74275bbae5e5f7ff718535dd3b
ruzen 01 <kim.vadim.alexandrovich@gmail.com>
2025-02-21 00:46:52 +0000

    add

--- resources/views/roles.blade.php
+++ resources/views/roles.blade.php
@@ -151,7 +151,7 @@
             <swiper-slide class="relative bg-white rounded-lg shadow-md overflow-hidden">
                 <div class="p-6 flex flex-col md:flex-row items-center">
                     <div class="w-full md:w-1/2 h-70 md:h-auto">
-                        <img src="{{ asset('images/roles/deputat.png') }}" alt="Диктатор" class="w-full h-full object-cover rounded-lg border border-gray-300">
+                        <img src="{{ asset('images/roles/diktator.png') }}" alt="Диктатор" class="w-full h-full object-cover rounded-lg border border-gray-300">
                     </div>
                     <div class="w-full md:w-1/2 md:pl-8 text-left">
                         <h3 class="text-xl text-center font-bold mb-4">Диктатор</h3>
