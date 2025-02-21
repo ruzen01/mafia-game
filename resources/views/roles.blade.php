59ac8badd6269f511544916931c509e0d0299b5f
ruzen 01 <kim.vadim.alexandrovich@gmail.com>
2025-02-21 05:35:24 +0000

    add

--- resources/views/roles.blade.php
+++ resources/views/roles.blade.php
@@ -168,10 +168,10 @@
             <swiper-slide class="relative bg-white rounded-lg shadow-md overflow-hidden">
                 <div class="p-6 flex flex-col md:flex-row items-center">
                     <div class="w-full md:w-1/2 h-70 md:h-auto">
-                        <img src="{{ asset('images/roles/deputat.png') }}" alt="Вор" class="w-full h-full object-cover rounded-lg border border-gray-300">
+                        <img src="{{ asset('images/roles/doktor.png') }}" alt="Доктор" class="w-full h-full object-cover rounded-lg border border-gray-300">
                     </div>
                     <div class="w-full md:w-1/2 md:pl-8 text-left">
-                        <h3 class="text-xl text-center font-bold mb-4">Депутат</h3>
+                        <h3 class="text-xl text-center font-bold mb-4">Доктор</h3>
                         <p class="text-gray-700 text-sm">
                             Сторона: <span class="bg-red-500 text-white px-1 rounded">Мирные</span><br>
                             Проверка: <span class="bg-gray-100 text-red-500 px-1 rounded">Мирный житель</span><br>
