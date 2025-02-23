9227d5f839ae08680e32be8b87b5ae71b064ad7f
ruzen 01 <kim.vadim.alexandrovich@gmail.com>
2025-02-23 23:17:30 +0000

    add

--- resources/views/roles.blade.php
+++ resources/views/roles.blade.php
@@ -11,8 +11,12 @@ $roles = json_decode(file_get_contents(resource_path('json/roles.json')), true);
 <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
     @foreach ($roles as $id => $role)
     <div class="cursor-pointer" onclick="openCard('{{ $id }}')">
-        <img src="{{ asset($role['image']) ?? 'images/roles/placeholder.png' }}" alt="{{ $role['title'] }}" class="w-full h-32 object-cover rounded-lg
-        border border-gray-300 transform transition-transform duration-300 hover:scale-105 hover:shadow-lg hover:sepia">
+        @php
+            // Проверяем, существует ли файл изображения
+            $imagePath = $role['image'] ? public_path('images/roles/' . $role['image']) : null;
+            $image = ($imagePath && file_exists($imagePath)) ? asset('images/roles/' . $role['image']) : asset('images/roles/placeholder.png');
+        @endphp
+        <img src="{{ $image }}" alt="{{ $role['title'] }}" class="w-full h-32 object-cover rounded-lg border border-gray-300 transform transition-transform duration-300 hover:scale-105 hover:shadow-lg hover:sepia">
         <p class="text-center mt-2">{{ $role['title'] }}</p>
     </div>
     @endforeach
