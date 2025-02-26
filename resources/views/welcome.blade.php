97fbc3ca3c1ace6d291b13509a132730d74bfb75
ruzen 01 <kim.vadim.alexandrovich@gmail.com>
2025-02-26 05:59:02 +0000

    add

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,8 +3,8 @@
 @section('content')
     <div class="flex items-center justify-center text-center">
         <div class="grid gap-4"> <!-- grid создает сетку, gap-4 добавляет отступы -->
-            <h1 class="text-5xl font-bold mb-4">Погрузитесь в мир Мафии</h1>
-            <p class="text-xl mb-8">Где каждый ход может стать последним</p>
+            <h1 class="text-5xl font-bold mb-4 bg-clip-text text-transparent bg-gradient-to-r from-gray-900 to-gray-400">Добро пожаловьть в мир городской мафии</h1>
+            <p class="text-xl mb-8 bg-clip-text text-transparent bg-gradient-to-r from-gray-900 to-gray-400">Где каждый ход может стать последним</p>
             <a href="{{ route('rules') }}" class="bg-gray-800 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded w-64 mx-auto">
                 Правила
             </a>
