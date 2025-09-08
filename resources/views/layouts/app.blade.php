1947760e94682c75581f7085ee2bf2e233f4e1a8
Kim Vadim <155291244+ruzen01@users.noreply.github.com>
2025-09-08 12:37:06 +1000

    1

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -51,7 +51,7 @@
     </div>
 
     <!-- Компактный футер -->
-    <footer class="w-full text-center text-white py-3 px-4 sm:px-6 bg-zinc-800 mt-auto">
+    <footer class="w-full text-center text-white py-1 px-4 sm:px-6 bg-zinc-800 mt-auto">
         mafia-vdk © 2025
     </footer>
 </body>
