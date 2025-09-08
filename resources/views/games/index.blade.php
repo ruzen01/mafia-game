0e505e7aa8410bbda7fb0bda11fe066451a79d2c
Kim Vadim <155291244+ruzen01@users.noreply.github.com>
2025-09-08 15:27:17 +1000

    1

--- resources/views/games/index.blade.php
+++ resources/views/games/index.blade.php
@@ -79,14 +79,22 @@
                     @can('update', [$game])
                     <td class="border border-zinc-500 w-28 px-1.5 py-2 text-center">
                         <div class="flex justify-center space-x-1">
-                            <a href="{{ route('games.edit', $game->id) }}" class="bg-yellow-500 text-white text-[10px] py-0.5 px-1.5 rounded hover:bg-yellow-600 transition whitespace-nowrap">
-                                ✏️
+                            <!-- Кнопка редактирования с SVG иконкой -->
+                            <a href="{{ route('games.edit', $game->id) }}" class="bg-yellow-500 text-white text-[10px] py-0.5 px-1.5 rounded hover:bg-yellow-600 transition whitespace-nowrap flex items-center justify-center w-6 h-6">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+                                  <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
+                                  <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
+                                </svg>
                             </a>
+                            <!-- Кнопка удаления с SVG иконкой -->
                             <form action="{{ route('games.destroy', $game->id) }}" method="POST" class="inline-block">
                                 @csrf
                                 @method('DELETE')
-                                <button type="submit" class="bg-red-500 text-white text-[10px] py-0.5 px-1.5 rounded hover:bg-red-600 transition whitespace-nowrap" onclick="return confirm('Удалить?')">
-                                    🗑️
+                                <button type="submit" class="bg-red-500 text-white text-[10px] py-0.5 px-1.5 rounded hover:bg-red-600 transition whitespace-nowrap flex items-center justify-center w-6 h-6" onclick="return confirm('Удалить?')">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                                      <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+                                      <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+                                    </svg>
                                 </button>
                             </form>
                         </div>
